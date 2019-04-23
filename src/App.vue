8d949f8c023eaf34c015f399d0e7d807f209fa06
parkej32 <parkej32@naver.com>
2019-04-24 00:40:37 +0900

    diary

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="portfolio">
+      Source : <a href="https://bitbucket.org/zzangsuni/diary-app/src" target="_blank">https://bitbucket.org/zzangsuni/diary-app/src</a>
+    </div>
     <section id="wrapper">
       <Header></Header>
       <Container></Container>
